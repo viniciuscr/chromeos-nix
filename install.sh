627db8740fada5696129ae2dfc92c11ace520a47
Vinicius CR <eu@viniciuscr.com.br>
2016-07-10 18:16:36 -0300

    lets give /usr/local/bin to chronos.
    it will make easier to update nix-init

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ wget -P ~/Downloads/ https://raw.githubusercontent.com/viniciuscr/chromeos-nix/m
 cd ~/Downloads/
 chmod +x nix-init
 sudo mkdir /usr/local/bin
-sudo mv nix-init /usr/local/bin/
+sudo chown chronos:chronos /usr/local/bin
+mv nix-init /usr/local/bin/
 nix-init
