a91e9d1171aa37c61151ef57fd5f95fb807a0d83
Vinicius CR <eu@viniciuscr.com.br>
2016-07-10 15:19:43 -0300

    fixed download path

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-wget https://raw.githubusercontent.com/viniciuscr/chromeos-nix/master/nix-init
+wget -P ~/Downloads/ https://raw.githubusercontent.com/viniciuscr/chromeos-nix/master/nix-init
+cd ~/Downloads/
 chmod +x nix-init
 sudo mkdir /usr/local/bin
 sudo mv nix-init /usr/local/bin/
