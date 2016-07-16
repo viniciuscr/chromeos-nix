6c298a781696380ad1557499d18d08da1ee4ccef
Vinicius CR <eu@viniciuscr.com.br>
2016-07-16 12:40:57 -0300

    dzã

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-wget -P ~/Downloads/ https://raw.githubusercontent.com/viniciuscr/chromeos-nix/master/nix-init
 cd ~/Downloads/
+wget -P ~/Downloads/ https://raw.githubusercontent.com/viniciuscr/chromeos-nix/master/nix-init
 chmod +x nix-init
 sudo mkdir /usr/local/bin
 sudo chown chronos:chronos /usr/local/bin
