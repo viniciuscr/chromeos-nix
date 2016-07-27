2b05c02c8e112ea5f1b104cc78c97584d26ae808
Vinicius CR <eu@viniciuscr.com.br>
2016-07-27 07:33:31 -0300

    Update install-native.sh

--- install-native.sh
+++ install-native.sh
@@ -11,3 +11,8 @@ sudo chown chronos:chronos /mnt/stateful_partition/nix-temp
 //enter nix or call it from proot
 nix-channel --update
 env NIX_STORE_DIR=/mnt/stateful_partition/nix-native TMPDIR=/mnt/stateful_partition/nix-temp nix-env -i nix
+//put binaries link on /usr/local/bin
+sudo ln -s /home/chronos/user/.nix-profile/bin /usr/local/bin
+//export variables on profile
+echo export "NIX_STORE_DIR='/mnt/stateful_partition/nix-native'" >> /home/chronos/user/.bash_profile
+echo export "TMPDIR='/mnt/stateful_partition/nix-temp'" >> /home/chronos/user/.bash_profile
